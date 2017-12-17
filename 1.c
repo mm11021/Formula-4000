8b1f1e6d124b3b4cd21d7c775949fde981843728
mm11021 <mm11021@alas.matf.bg.ac.rs>
2017-12-17 11:49:42 +0100

    Dodate nove funkcionalnosti u C programu.

--- 1.c
+++ 1.c
@@ -5,15 +5,20 @@
 #include <stdarg.h>
 #include <errno.h>
 
+#define QUERY_SIZE 513
 
-#define QUERY_SIZE 260
+#define BUFFER_SIZE 46
 
-#define BUFFER_SIZE 80
+#define BROJ_OPCIJA 3
 
 /* Funkcija error_fatal() ispisuje poruku o gresci i potom prekida program. */
 static void error_fatal(char *format,...);
 
 void ispisi(MYSQL_RES *rezultat);
+
+int ponudi_opcije(int opcija);
+
+void formiraj_upit(int p,char *query,MYSQL *konekcija);
 
 int main(int argc,char *argv[])
 {
@@ -21,7 +26,6 @@ int main(int argc,char *argv[])
   MYSQL_RES *rezultat;	/* Promenljiva za rezultat. */
 
   char query[QUERY_SIZE];	/* Promenljiva za formuaciju upita. */
-  char buffer[BUFFER_SIZE];	/* Velicina poruke koja se ucitava sa ulaza. */
 
   /* Incijalizuje se promenljiva koja ce predstavljati konekciju. */
   konekcija = mysql_init(NULL);
@@ -34,82 +38,41 @@ int main(int argc,char *argv[])
 
   while(1)
   {
-    int opcija=1;
+    int opcija=1,p;
     printf("\n");
     printf("Odaberite jednu od sledecih opcija (0 za kraj programa):\n");
-    printf("%d. Ispis formula\n",opcija++);
-    printf("%d. Ispis vozaca\n",opcija++);
-    printf("%d. Ispis menadzera\n",opcija++);
-    printf("%d. Ispis sponzora\n",opcija++);
-    printf("%d. Ispis vlasnika\n",opcija++);
-    printf("%d. Ispis mehanicara\n",opcija++);
-    printf("%d. Ispis timova i vozaca\n",opcija++);
-    printf("%d. Ispis formula, timova i vozaca\n",opcija++);
-    printf("%d. Ispis informacija o gumama\n",opcija++);
-    printf("%d. Ispis informacija o motorima na formulama\n",opcija++);
-    printf("%d. Ispis sponzora timova\n",opcija++);
-    printf("%d. Ispis vlasnika timova\n",opcija++);
-    printf("%d. Ispis mehanicara timova\n",opcija++);
+    printf("%d. Ispis podataka\n",opcija++);
+    printf("%d. Unos podataka\n", opcija++);
+    printf("%d. Izmena podataka\n", opcija++);
     scanf("%d",&opcija);
     printf("\n");
 
     if(opcija==0)
       break;
-    switch(opcija)
-    {
-      case 1:
-        sprintf(query,"select * from formula");
-        break;
-      case 2:
-        sprintf(query,"select * from vozac");
-        break;
-      case 3:
-        sprintf(query,"select * from menadzer");
-        break;
-      case 4:
-        sprintf(query,"select * from sponzor");
-        break;
-      case 5:
-        sprintf(query,"select * from vlasnik");
-        break;
-      case 6:
-        sprintf(query,"select * from mehanicar");
-        break;
-      case 7:
-        sprintf(query,"select id_tima,t.ime as ime_tima,v.ime as ime_vozaca,prezime as prezime_vozaca from tim as t join vozac as v on id_tima=tim");
-        break;
-      case 8:
-        sprintf(query,"select broj_sasije,proizvodjac,maks_brzina,t.ime as ime_tima,v.ime as ime_vozaca,prezime as prezime_vozaca from formula as f join tim as t on f.tim=id_tima join vozac as v on broj_sasije=formula");
-        break;
-      case 9:
-        sprintf(query,"select broj_sasije,f.proizvodjac as proizvodjac_formule,maks_brzina,pg.proizvodjac as proizvodjac_guma,tip,velicina,radna_temperatura,trajanje from formula as f join paket_guma as pg on broj_sasije=formula");
-        break;
-      case 10:
-        sprintf(query,"select broj_sasije,f.proizvodjac as proizvodjac_formule,maks_brzina,m.proizvodjac as proizvodjac_motora,zapremina,radna_temperatura,snaga,broj_obrtaja,pouzdanost from formula as f join motor as m on motor=broj_motora");
-        break;
-      case 11:
-        sprintf(query,"select t.ime as ime_tima,s.ime as ime_sponzora,marketing,brend,trajanje from tim as t join sponzorski_pul as sp on t.id_tima=sp.id_tima join sponzor as s on s.id_sponzora=sp.id_sponzora order by t.id_tima");
-        break;
-      case 12:
-        sprintf(query,"select t.ime as ime_tima,v.ime as ime_vlasnika,v.prezime as prezime_vlasnika,saldo,kriminalna_proslost,redovna_isplata,trajanje from tim as t join vlasnistvo as vl on t.id_tima=vl.id_tima join vlasnik as v on v.id_vlasnika=vl.id_vlasnika order by t.id_tima");
-        break;
-      case 13:
-        sprintf(query,"select t.ime as ime_tima,m.ime as ime_mehanicara,prezime as prezime_mehanicara,struka,staz,prethodni_poslovi,poslednji_servis from tim as t join odrzavanje as o on t.id_tima=o.id_tima join mehanicar as m on broj_licence=id_mehanicara order by t.id_tima");
-        break;
-      default:
-        printf("Pogresna opcija.\n");
-        continue;
-    }
+
+    p=ponudi_opcije(opcija);
+
+    if(p==-1)
+      continue;
+
+    formiraj_upit(p,query,konekcija);
 
     /* Pokusava se sa izvrsavanjem upita. */
     if(mysql_query(konekcija, query) != 0)
       error_fatal("Greska u upitu %s\n", mysql_error(konekcija));
 
-    rezultat = mysql_use_result(konekcija);
-
-    ispisi(rezultat);
-
-    mysql_free_result(rezultat);
+    switch(opcija)
+    {
+      case 1:
+        rezultat = mysql_use_result(konekcija);
+        ispisi(rezultat);
+        mysql_free_result(rezultat);
+        break;
+      case 2:
+        if(mysql_affected_rows(konekcija)==1)
+          printf("Uspesno je unet podatak u bazu.\n");
+        break;
+    }
   }
   
   /* Oslobadja se trenutni rezultat, posto nam vise ne treba. */
@@ -120,6 +83,164 @@ int main(int argc,char *argv[])
 
   /* Zavrsava se program */
   exit(EXIT_SUCCESS);
+}
+
+void formiraj_upit(int p,char *query,MYSQL *konekcija)
+{
+  char buffer1[BUFFER_SIZE],buffer2[BUFFER_SIZE],buffer3[BUFFER_SIZE],buffer4[BUFFER_SIZE];
+  int a,b,c,d,e,f;
+  MYSQL_RES *rezultat;
+  switch(p%BROJ_OPCIJA)
+  {
+    case 0:
+      switch(p/BROJ_OPCIJA)
+      {
+        case 1:
+          sprintf(query,"select * from formula");
+          break;
+        case 2:
+          sprintf(query,"select * from vozac");
+          break;
+        case 3:
+          sprintf(query,"select * from menadzer");
+          break;
+        case 4:
+          sprintf(query,"select * from sponzor");
+          break;
+        case 5:
+          sprintf(query,"select * from vlasnik");
+          break;
+        case 6:
+          sprintf(query,"select * from mehanicar");
+          break;
+        case 7:
+          sprintf(query,"select id_tima,t.ime as ime_tima,v.ime as ime_vozaca,prezime as prezime_vozaca from tim as t join vozac as v on id_tima=tim");
+          break;
+        case 8:
+          sprintf(query,"select broj_sasije,proizvodjac,maks_brzina,t.ime as ime_tima,v.ime as ime_vozaca,prezime as prezime_vozaca from formula as f join tim as t on f.tim=id_tima join vozac as v on broj_sasije=formula");
+          break;
+        case 9:
+          sprintf(query,"select broj_sasije,f.proizvodjac as proizvodjac_formule,maks_brzina,pg.proizvodjac as proizvodjac_guma,tip,velicina,radna_temperatura,trajanje from formula as f join paket_guma as pg on broj_sasije=formula");
+          break;
+        case 10:
+          sprintf(query,"select broj_sasije,f.proizvodjac as proizvodjac_formule,maks_brzina,m.proizvodjac as proizvodjac_motora,zapremina,radna_temperatura,snaga,broj_obrtaja,pouzdanost from formula as f join motor as m on motor=broj_motora");
+          break;
+        case 11:
+          sprintf(query,"select t.ime as ime_tima,s.ime as ime_sponzora,marketing,brend,trajanje from tim as t join sponzorski_pul as sp on t.id_tima=sp.id_tima join sponzor as s on s.id_sponzora=sp.id_sponzora order by t.id_tima");
+          break;
+        case 12:
+          sprintf(query,"select t.ime as ime_tima,v.ime as ime_vlasnika,v.prezime as prezime_vlasnika,saldo,kriminalna_proslost,redovna_isplata,trajanje from tim as t join vlasnistvo as vl on t.id_tima=vl.id_tima join vlasnik as v on v.id_vlasnika=vl.id_vlasnika order by t.id_tima");
+          break;
+        case 13:
+          sprintf(query,"select t.ime as ime_tima,m.ime as ime_mehanicara,prezime as prezime_mehanicara,struka,staz,prethodni_poslovi,poslednji_servis from tim as t join odrzavanje as o on t.id_tima=o.id_tima join mehanicar as m on broj_licence=id_mehanicara order by t.id_tima");
+          break;
+        default:
+          printf("Pogresna opcija.\n");
+          return;
+      }
+       break;
+    case 1:
+      switch(p/BROJ_OPCIJA)
+      {
+        case 1:
+          if(mysql_query(konekcija, "select broj_sasije from formula order by 1") != 0)
+            error_fatal("Greska u upitu %s\n", mysql_error(konekcija));
+          rezultat = mysql_use_result(konekcija);
+          ispisi(rezultat);
+          mysql_free_result(rezultat);
+
+          printf("Unesite broj sasije (ne sme biti neki od gore navedenih): ");
+          scanf("%d",&a);
+          printf("Unesite proizvodjaca: ");
+          scanf("%s",buffer1);
+          printf("Unesite maksimalnu brzinu: ");
+          scanf("%d",&b);
+          printf("Unesite tezinu: ");
+          scanf("%d",&c);
+          printf("Unesite duzinu: ");
+          scanf("%d",&d);
+          
+          if(mysql_query(konekcija, "select * from motor where broj_motora not in (select motor from formula)") != 0)
+            error_fatal("Greska u upitu %s\n", mysql_error(konekcija));
+          rezultat = mysql_use_result(konekcija);
+          ispisi(rezultat);
+          mysql_free_result(rezultat);
+
+          printf("Unesite broj motora (ponudjeni motori su slobodni): ");
+          scanf("%d",&e);
+          
+          if(mysql_query(konekcija, "select * from tim order by id_tima") != 0)
+            error_fatal("Greska u upitu %s\n", mysql_error(konekcija));
+          rezultat = mysql_use_result(konekcija);
+          ispisi(rezultat);
+          mysql_free_result(rezultat);
+
+          printf("Unesite identifikator tima: ");
+          scanf("%d",&f);
+
+          sprintf(query,"insert into formula values (%d,'%s',%d,%d,%d,%d,%d)",a,buffer1,b,c,d,e,f);
+          break;
+        case 2:
+          break;
+      }
+      break;
+    case 2:
+      switch(p/BROJ_OPCIJA)
+      {
+        case 1:
+          break;
+      }
+    default:
+      printf("Ovo je neka magija\n");
+      return;
+  }
+}
+
+int ponudi_opcije(int opcija)
+{
+  int p,pom;
+  switch(opcija)
+  {
+    case 0:
+      return 0;
+    case 1:
+      pom=1;
+      printf("%d. Ispis formula\n",pom++);
+      printf("%d. Ispis vozaca\n",pom++);
+      printf("%d. Ispis menadzera\n",pom++);
+      printf("%d. Ispis sponzora\n",pom++);
+      printf("%d. Ispis vlasnika\n",pom++);
+      printf("%d. Ispis mehanicara\n",pom++);
+      printf("%d. Ispis timova i vozaca\n",pom++);
+      printf("%d. Ispis formula, timova i vozaca\n",pom++);
+      printf("%d. Ispis informacija o gumama\n",pom++);
+      printf("%d. Ispis informacija o motorima na formulama\n",pom++);
+      printf("%d. Ispis sponzora timova\n",pom++);
+      printf("%d. Ispis vlasnika timova\n",pom++);
+      printf("%d. Ispis mehanicara timova\n",pom++);
+      scanf("%d",&pom);
+      printf("\n");
+      p=BROJ_OPCIJA*pom;
+      break;
+    case 2:
+      pom=1;
+      printf("%d. Unos formule\n",pom++);
+      printf("%d. Unos vozaca\n",pom++);
+      printf("%d. Unos menadzera\n",pom++);
+      printf("%d. Unos sponzora\n",pom++);
+      printf("%d. Unos vlasnika\n",pom++);
+      printf("%d. Unos mehanicara\n",pom++);
+      printf("%d. Unos tima\n",pom++);
+      scanf("%d",&pom);
+      printf("\n");
+      p=BROJ_OPCIJA*pom+1;
+      break;
+    default:
+      printf("Pogresna opcija!\n");
+      p=-1;
+      break;
+  }
+  return p;
 }
 
 void ispisi(MYSQL_RES *rezultat)
